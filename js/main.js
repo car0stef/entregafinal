d4413215db08e2ab6a232a8e978768caf42addc9
car0stef <stefani.aishwarya@gmail.com>
2022-05-12 22:28:04 -0300

    Incorporar Eventos

--- js/main.js
+++ js/main.js
@@ -1,89 +1,44 @@
-//Funcion para preguntar modelo de remera de Niño
-//Esta funcion está incluida en la funcion totalNinio
-function preguntaN(){
-    const modeloRemerasNinio = [
-        {codigo: "1", modelo: "Batman"},
-        {codigo: "2", modelo: "Hulk"},
-        {codigo: "3", modelo: "Superman"}
-    ];
-        let preguntaNinio = prompt(`Ahora ¿Qué modelito desea seleccionar?\n
-            ${modeloRemerasNinio[0].modelo}\n
-            ${modeloRemerasNinio[1].modelo}\n
-            ${modeloRemerasNinio[2].modelo}\n
-            Presione ${modeloRemerasNinio[0].codigo}, ${modeloRemerasNinio[1].codigo} o ${modeloRemerasNinio[2].codigo} para seleccionar`);
+const divMain = document.getElementById("divMain");
 
-        
-        let resultNinio = modeloRemerasNinio.find(x => x.codigo === preguntaNinio);
-            return alert(` Su modelo seleccionado es: ${resultNinio.modelo}`);
+const remerasAdulto = [
+    {codigo: "1" , modelo: "Aries", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "2" , modelo: "Géminis", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "3" , modelo: "Leo", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "4" , modelo: "Capricornio", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "5" , modelo: "Piscis", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "6" , modelo: "Sagitario", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "7" , modelo: "Escorpio", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "8" , modelo: "Cáncer", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "9" , modelo: "Tauro", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "10" , modelo: "Virgo", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "11" , modelo: "Acuario", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+    {codigo: "12" , modelo: "Libra", precio:2300, imagen: "<img src=./images/remera.jpg> </img>"},
+];
+
+const card = (remerasAdulto) =>{
+    return `
+        <div class="card mx-4 my-4" style="width: 15rem;">
+            <img > ${remerasAdulto.imagen} </img>
+                <div class="card-body">
+                    <h2 class="h5"> ${remerasAdulto.modelo}</h2>
+                    <p class="card-text"> Código: ${remerasAdulto.codigo} </p>
+                    <a href= "#" onclick = boton(${remerasAdulto.codigo}) class="btn btn-primary">Comprar producto </a>
+                </div>
+        </div> `
 }
 
-function totalNinio(cuanto){
-                cuanto = parseInt(prompt("¿Cuántas remeras desea comprar?"));
-                    let resultado = cuanto * 1500;
-                        preguntaN();
-                            return alert(`Precio unitario: 1500$ \n El total de su compra es: ${resultado}$`);
+const productos =(remeras, nodoDivMain)=>{
+    let acumulador = "";
+    remeras.forEach((remeraAdulto) => {
+        acumulador = acumulador + card(remeraAdulto);
+    })
+    nodoDivMain.innerHTML=acumulador;
+};
+
+function boton(codigo, modelo) {
+    const encontrar = remerasAdulto.find(item => item.codigo == codigo);
+    alert("Agregaste el artículo Nº " + encontrar.codigo);
 }
+productos(remerasAdulto, divMain);
 
 
-//Funcion para preguntar modelo de remera de Adulto
-//Esta funcion está incluida en la funcion totalAdulto
-function preguntaA(){
-    const modeloRemerasAdulto = [
-        {codigo: "1" , modelo: "Aries"},
-        {codigo: "2" , modelo: "Géminis"},
-        {codigo: "3" , modelo: "Capricornio"}
-    ];  
-        let preguntaAdulto = prompt(`Ahora ¿Qué modelo desea seleccionar?\n
-            ${modeloRemerasAdulto[0].modelo} con código ${modeloRemerasAdulto[0].codigo}\n
-            ${modeloRemerasAdulto[1].modelo} con código ${modeloRemerasAdulto[1].codigo}\n 
-            ${modeloRemerasAdulto[2].modelo} con código ${modeloRemerasAdulto[2].codigo}\n`);
-                let resultAdulto = modeloRemerasAdulto.find(x => x.codigo === preguntaAdulto);
-                    if(resultAdulto === "1" || "2" || "3"){
-                        alert(` Su modelo seleccionado es: ${resultAdulto.modelo}`);
-                    }else{
-
-                        //Este no me anda y no entiendo el porque =(
-                        //Igualmente el resto funciona bien
-
-                        return alert("Lo sentimos usted debe seleccionar entre: 1, 2 y 3");
-                    }
-}
-
-function totalAdulto(cuanto){
-    cuanto = parseInt(prompt("¿Cuántas remeras desea comprar?"));
-        let resultado = cuanto * 2300;
-            preguntaA();
-                return alert(`Precio unitario: 2300$ \n El total de su compra es: ${resultado} $`);
-}
-
-function saludo(){
-    alert("Gracias por su compra. Vuelva Pronto");
-}
-
-//Aqui empieza el programa
-function talle(){
-    let tipoPersona = prompt("Comprar remeras para: Niño (1) Adulto (2)");    
-    
-        if (tipoPersona == "1") {
-            let ninio = prompt("¿Qué talle desea seleccionar? 4 , 6 , 8 , 10");
-                if((ninio == "4") || (ninio == "6") || (ninio == "8") || (ninio == "10")){
-                    alert(`Se agregó correctamente el talle ${ninio}`);                    
-                    totalNinio();
-                    saludo();
-                } else{
-                    alert("Ud. necesita ingresar un talle válido");
-                }
-            }
-            else if (tipoPersona == "2"){
-            let adulto = prompt("¿Qué talle desea seleccionar? S, M, L, XL");
-                if((adulto == "s") || (adulto == "m") || (adulto == "l") || (adulto == "xl") || (adulto == "S") || (adulto == "M") || (adulto == "L") || (adulto == "XL")){
-                    alert(`Se agregó correctamente el talle ${adulto}`);
-                    totalAdulto();
-                    saludo();
-                }else{
-                    alert("Debes ingresar un talle existente");
-                }
-            }
-}
-
-talle();
